21a812d73d9774d2f1ecaafc4b2fced59240f207
Eugen <kovalchuk.eak@gmail.com>
2022-08-13 02:59:31 +0300

    fix

--- src/components/wallet/Wallet.jsx
+++ src/components/wallet/Wallet.jsx
@@ -12,32 +12,35 @@ export const Wallet = () => {
       sx={{
         width: { xs: "100%", sm: "600px", md: "900px", lg: "1200px" },
         padding: "15px",
-        marginBottom: "15px",
+        display: "flex",
+        justifyContent: "center",
       }}
     >
-      <Typography sx={{ mb: "10px", mr: "10px" }}>Ballance</Typography>
-      <Box sx={{ marginBottom: "15px" }}>
-        <AmountCards />
-        <AmountCash />
-      </Box>
       <Box>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Typography sx={{ mr: "124px" }}>Cards</Typography>
-          <Button
-            variant="contained"
-            size="small"
-            onClick={() => setModalAddCard(true)}
-          >
-            Add card
-          </Button>
+        <Typography sx={{ mb: "10px", mr: "10px" }}>Ballance</Typography>
+        <Box sx={{ marginBottom: "15px" }}>
+          <AmountCards />
+          <AmountCash />
         </Box>
-        {modalAddCard && (
-          <ModalAddCard
-            modalAddCard={modalAddCard}
-            setModalAddCard={setModalAddCard}
-          />
-        )}
-        <CardsList />
+        <Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography sx={{ mr: "124px" }}>Cards</Typography>
+            <Button
+              variant="contained"
+              size="small"
+              onClick={() => setModalAddCard(true)}
+            >
+              Add card
+            </Button>
+          </Box>
+          {modalAddCard && (
+            <ModalAddCard
+              modalAddCard={modalAddCard}
+              setModalAddCard={setModalAddCard}
+            />
+          )}
+          <CardsList />
+        </Box>
       </Box>
     </Box>
   );
